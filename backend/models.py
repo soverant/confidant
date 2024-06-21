1f482e410a9984a7aba35488eeec1df2f2d0abc8
Amirhossein Nouranizadeh <anouranizadeh@interrodata.com>
2024-06-21 18:30:24 +0330

    NodeModel and EdgeModel implemented.

--- backend/models.py
+++ backend/models.py
@@ -7,15 +7,44 @@ from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 
+class TimestampMixin:
+    created_at = fields.DatetimeField(null=True, auto_now_add=True)
+    modified_at = fields.DatetimeField(null=True, auto_now=True)
+
+
+class NodeModel(models.Model, TimestampMixin):
+    id = fields.UUIDField(primary_key=True)
+    spec = fields.data.TextField()
+    prompt = fields.data.TextField()
+    response = fields.data.TextField()
+
+    in_edges = fields.ReverseRelation["EdgeModel"]
+    out_edges = fields.ReverseRelation["EdgeModel"]
+
+    async def children_ids(self):
+        return await self.out_edges.all().values_list('tail', flat=True)
+
+    async def parents_ids(self):
+        return await self.in_edges.all().values_list('head', flat=True)
+
+    class PydanticMeta:
+        computed = ["parents_ids", "children_ids"]
+
+
+class EdgeModel(models.Model, TimestampMixin):
+    id = fields.UUIDField(primary_key=True)
+    head: fields.ForeignKeyRelation[NodeModel] = fields.ForeignKeyField(
+        "models.NodeModel", related_name="out_edges", to_field="id"
+    )
+    tail: fields.ForeignKeyRelation[NodeModel] = fields.ForeignKeyField(
+        "models.NodeModel", related_name="in_edges", to_field="id"
+    )
+
+
 class SenderTypeEnum(str, Enum):
     USER = 'user'
     CONFIDANT = 'confidant'
     SYSTEM = 'system'
-
-
-class TimestampMixin():
-    created_at = fields.DatetimeField(null=True, auto_now_add=True)
-    modified_at = fields.DatetimeField(null=True, auto_now=True)
 
 
 class TortoiseAbstractBaseModel(models.Model):
@@ -41,7 +70,15 @@ class Messages(TortoiseAbstractBaseModel, TimestampMixin):
 class SuccessDTO(PydanticBaseModel):
     msg: Optional[str]
 
+
 Tortoise.init_models(["models"], "models")
 Chat = pydantic_model_creator(Chats, name="Chat")
 Message = pydantic_model_creator(Messages, name="Message")
 MessageIn = pydantic_model_creator(Messages, name="Message", exclude=["chat"])
+
+Node = pydantic_model_creator(NodeModel, name="Node")
+NodeIn = pydantic_model_creator(NodeModel, name="NodeIn",
+                                exclude=("id", "in_edges", "out_edges"),
+                                exclude_readonly=True)
+EdgeIn = pydantic_model_creator(EdgeModel, name="Edge", exclude=("id",), exclude_readonly=True)
+
