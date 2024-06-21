9acb63719cb2cd6b13d38957c8f34446f906d24d
Amirhossein Nouranizadeh <anouranizadeh@interrodata.com>
2024-06-21 18:30:34 +0330

    ChatService commented.

--- backend/services.py
+++ backend/services.py
@@ -22,13 +22,13 @@ class NarratorConfidant:
                        content="Hello world!")
 
 
-class ChatService:
-    def create_chat(self, confidant: NarratorConfidant)->Chat:
-        chat_id = uuid.uuid4()
-        chat_obj = await Chats.create(id=chat_id)
-        rep_msg = confidant.chat(chat_obj)
-        msg_obj = await Messages.create(**rep_msg.model_dump(exclude_unset=True,exclude="chat"),chat=chat_obj)
-        response.set_cookie(key=CHAT_COOKIE_KEY, value=str(chat_id))
-        data = await Chat.from_queryset_single(Chats.get(id=chat_id))
-        return chat
+# class ChatService:
+#     def create_chat(self, confidant: NarratorConfidant)->Chat:
+#         chat_id = uuid.uuid4()
+#         chat_obj = await Chats.create(id=chat_id)
+#         rep_msg = confidant.chat(chat_obj)
+#         msg_obj = await Messages.create(**rep_msg.model_dump(exclude_unset=True,exclude="chat"),chat=chat_obj)
+#         response.set_cookie(key=CHAT_COOKIE_KEY, value=str(chat_id))
+#         data = await Chat.from_queryset_single(Chats.get(id=chat_id))
+#         return chat
 
