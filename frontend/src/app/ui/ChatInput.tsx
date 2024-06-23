39e657d21212b55f78a7641525eefbd9b03a10a8
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-23 18:44:29 +0330

    adding props and logic for calling send message

--- frontend/src/app/ui/ChatInput.tsx
+++ frontend/src/app/ui/ChatInput.tsx
@@ -9,32 +9,45 @@ import {
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
-interface ChatInputProps {}
+interface ChatInputProps {
+  actions?: any[];
+  text?: string;
+  onTextChange?: (text: string) => void;
+  onSend: () => void;
+}
 
 export const ChatInput: FC<ChatInputProps> = (props) => {
+  const { actions, text, onTextChange, onSend } = props;
   return (
     <Box className="p-2 flex flex-col justify-center">
-      <Box className="p-10  flex flex-col justify-center">
-        <Button variant="contained" href="#contained-buttons">
-          Actions
-        </Button>
-      </Box>
+      {!!actions && actions.length > 0 && (
+        <Box className="p-10  flex flex-col justify-center">
+          <Button variant="contained" href="#contained-buttons">
+            Actions
+          </Button>
+        </Box>
+      )}
       <FormControl variant="outlined" fullWidth>
         <OutlinedInput
           multiline
           rows={2}
           id="outlined-send-message"
+          value={text}
+          onChange={(e) => {
+            onTextChange && onTextChange(e.target.value);
+          }}
           endAdornment={
             <InputAdornment position="end" className="self-start pt-2">
               <IconButton
                 aria-label="send"
-                // edge="end"
+                onClick={() => {
+                  onSend();
+                }}
               >
                 <SendIcon />
               </IconButton>
             </InputAdornment>
           }
-          //   label="Password"
         />
       </FormControl>
     </Box>
