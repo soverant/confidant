ebb8e70a1a6b5df497c6c09861b583ba2454aff4
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-23 18:41:44 +0330

    improve api for usage in app

--- backend/chats/chat.py
+++ backend/chats/chat.py
@@ -1,4 +1,3 @@
-
 import logging
 import uuid
 from fastapi import APIRouter, HTTPException, Response, Cookie
@@ -17,15 +16,15 @@ router = APIRouter(
 
 confidants_service = ConfidantsService()
 
-@router.post("/{confidant_id}/create")
+
+@router.get("/create/{confidant_id}")
 async def create_chat(response: Response, confidant_id: str) -> Chat:
     try:
         confidant = confidants_service.get_confidant(confidant_id=confidant_id)
         chat_id = uuid.uuid4()
-        chat_obj = await Chats.create(id=chat_id,confidant_id=confidant_id)
+        chat_obj = await Chats.create(id=chat_id, confidant_id=confidant_id)
         rep_msg = confidant.chat(chat_obj)
-        msg_obj = await Messages.create(**rep_msg.model_dump(exclude_unset=True,exclude="chat"),chat=chat_obj)
-        response.set_cookie(key=CHAT_COOKIE_KEY, value=str(chat_id))
+        msg_obj = await Messages.create(**rep_msg.model_dump(exclude_unset=True, exclude="chat"), chat=chat_obj)
         data = await Chat.from_queryset_single(Chats.get(id=chat_id))
         log.debug(data)
         return data
@@ -45,14 +44,14 @@ async def create_chat(response: Response, confidant_id: str) -> Chat:
         )
 
 
-@router.get("/")
-async def get_chat(chat_id: Annotated[Union[str, None], Cookie()] = None) -> Chat:
+@router.get("/{chat_id}")
+async def get_chat(chat_id: str) -> Chat:
     try:
         log.debug("request chats with confidant: %s  chat_id: %s")
-        if chat_id == None:
-            raise ValueError("chat id not found")    
+        if chat_id is None:
+            raise ValueError("chat id not found")
         chat = await Chat.from_queryset_single(Chats.get(id=chat_id))
-        return chat 
+        return chat
     except ValueError as e:
         err = str(e)
         log.error("inputs are not valid: %s", err)
@@ -70,28 +69,19 @@ async def get_chat(chat_id: Annotated[Union[str, None], Cookie()] = None) -> Cha
 
 
 @router.post("/")
-async def send(req: MessageIn, chat_id: Annotated[Union[str, None], Cookie()] = None) -> Chat:
+async def send(req: MessageIn) -> Chat:
     log.debug("request chats with confidant: %s  chat_id: %s")
-    if chat_id == None:
-        log.error("chat_id missing: %s", err)
+    chat_obj = await Chats.get(id=req.chat_id)
+    await Messages.create(**req.model_dump(exclude_unset=True), chat=chat_obj, id=uuid.uuid4())
+    if chat_obj is None:
+        log.error("chat not found")
         raise HTTPException(
             status_code=404,
-            detail=err
-        )  
-    chat_obj = await Chats.get(id=chat_id)
-    in_msg_obj = await Messages.create(**req.model_dump(exclude_unset=True),chat=chat_obj,id=uuid.uuid4())
-    if chat_obj == None:
-        log.error("chat not found: %s", err)
-        raise HTTPException(
-            status_code=404,
-            detail=err
-        )  
-    log.debug("confidant %d",chat_obj.confidant_id)
-    confidant = confidants_service.get_confidant(confidant_id=str(chat_obj.confidant_id))    
+            detail="chat not found"
+        )
+    log.debug("confidant %d", chat_obj.confidant_id)
+    confidant = confidants_service.get_confidant(confidant_id=str(chat_obj.confidant_id))
     rep_msg = confidant.chat(chat_obj)
-    msg_obj = await Messages.create(**rep_msg.model_dump(exclude_unset=True,exclude=["chat"]),chat=chat_obj)
-    data = await Chat.from_queryset_single(Chats.get(id=chat_id))
-    return data 
-
-
-
+    await Messages.create(**rep_msg.model_dump(exclude_unset=True, exclude=["chat"]), chat=chat_obj)
+    data = await Chat.from_queryset_single(Chats.get(id=req.chat_id))
+    return data
