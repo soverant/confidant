02ee16b15e42b0818e78acbe48be9dbc338585e9
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-23 18:46:06 +0330

    include chat_id in model

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -44,5 +44,5 @@ class SuccessDTO(PydanticBaseModel):
 Tortoise.init_models(["chats.models"], "models")
 Chat = pydantic_model_creator(Chats, name="Chat")
 Message = pydantic_model_creator(Messages, name="Message")
-MessageIn = pydantic_model_creator(Messages, name="MessageIn", exclude=["chat","chat_id","id"])
+MessageIn = pydantic_model_creator(Messages, name="MessageIn", exclude=["chat","id"])
 
