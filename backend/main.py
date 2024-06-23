85645df7c3bcf26bed9eabb24f9f03986017ad0f
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-23 18:45:39 +0330

    handling cors for development

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,15 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import RegisterTortoise
 from contextlib import asynccontextmanager
 from logger import setup_logger
-from config import get_config
 from poset import poset
+from config import get_config
 from poset.repository import get_database
 from chats import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 conf = get_config()
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     # app startup
@@ -40,18 +43,27 @@ try:
     app = FastAPI(title="Soverant POC API", lifespan=lifespan)
     app.include_router(poset.router)
     app.include_router(chat.router)
+    # Allow all origins (for development purposes)
+    if conf.is_dev:
+        origins = [
+            "*"
+        ]
+
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
 except Exception as e:
     print("the app is ended with this error: %s", str(e))
     sys.exit(1)
 
-
-
-
-
-
 if __name__ == "__main__":
     import uvicorn
+
     log.info("API is ready on http://%s:%d", conf.HOST, conf.PORT)
     log.info("Docs available is ready on http://%s:%d/docs", conf.HOST, conf.PORT)
     uvicorn.run("main:app", host=conf.HOST, port=conf.PORT, reload=conf.RELOAD, workers=2)
