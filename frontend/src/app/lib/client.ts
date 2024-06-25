ed776c875779948876e7b6b8d68230bc370734a3
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-25 21:35:56 +0330

    Config client service

---
+++ frontend/src/app/lib/client.ts
@@ -0,0 +1,6 @@
+
+import * as Services from "./generated-client/services.gen"
+import * as Types from "./generated-client/types.gen"
+import { OpenAPI } from "./generated-client";
+OpenAPI.BASE = 'http://localhost:8000';
+export {Services}
