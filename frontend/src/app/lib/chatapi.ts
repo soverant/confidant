b7c8ef833e287071add74b731a36c3651134e342
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-25 16:29:33 +0330

    add openapi spec and generated client

---
+++ frontend/src/app/lib/chatapi.ts
@@ -0,0 +1,69 @@
+// postChat.ts
+
+// Define the data interface
+export interface NewMessage {
+    chat_id: string
+    content: string
+    sender_type: "user"|"system"|"confidant"
+    sender: string
+}
+export interface Message {
+    id: string,
+    created_at: Date
+    modified_at: Date
+    content: string
+    sender_type: "user"|"system"|"confidant"
+    sender: string
+}
+export interface ChatData {
+    id: string;
+    confidant_id: string
+    messages: Message[]
+}
+
+// Function to post data
+export async function sendMessage(url: string, data: NewMessage): Promise<ChatData> {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result: ChatData = await response.json();
+        console.log('Success:', result);
+        return result
+    } catch (error) {
+        console.error('Error:', error);
+        throw "send message failed"
+    }
+}
+
+export async function getChatData(url: string): Promise<ChatData> {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result: ChatData = await response.json();
+        console.log('Chat Data:', result);
+        return result;
+    } catch (error) {
+        console.error('Error:', error);
+        throw "chat not found"
+    }
+}
+
